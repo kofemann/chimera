7f769a806997344ae7c707f852cd4cc0c4ece70a
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2011-03-02 15:16:32 +0100

    use liquibase for db management.
    drop custom xml files
    update junit tests

--- test/org/dcache/chimera/ChimeraTestCaseHelper.java
+++ test/org/dcache/chimera/ChimeraTestCaseHelper.java
@@ -1,12 +1,15 @@
 package org.dcache.chimera;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import org.dcache.chimera.util.SqlHelper;
+import com.mchange.v2.c3p0.DataSources;
+import java.io.FileReader;
+import java.util.Properties;
+import javax.sql.DataSource;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import org.junit.After;
 import org.junit.Before;
 
@@ -19,31 +22,28 @@ public abstract class ChimeraTestCaseHelper {
     @Before
     public void setUp() throws Exception {
 
-        Class.forName("org.hsqldb.jdbcDriver");
+        Properties dbProperties = new Properties();
+        dbProperties.load(new FileReader("chimera-test.properties"));
 
-        _conn = DriverManager.getConnection("jdbc:hsqldb:mem:chimeramem", "sa", "");
+        Class.forName(dbProperties.getProperty("chimera.db.driver"));
+        DataSource dataSource = DataSources.unpooledDataSource(dbProperties.getProperty("chimera.db.url"),
+                dbProperties.getProperty("chimera.db.user"), dbProperties.getProperty("chimera.db.password"));
+
+        _conn = dataSource.getConnection();
         _conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 
-        File sqlFile = new File("sql/create-hsqldb.sql");
-        StringBuilder sql = new StringBuilder();
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(_conn));
+        Liquibase liquibase = new Liquibase("org/dcache/chimera/changelog/changelog-master.xml",
+                new ClassLoaderResourceAccessor(), database);
+        // Uncomment the following line when testing with mysql database
+      /*
+         * Liquibase liquibase = new Liquibase(changeLogFile, new
+         * ClassLoaderResourceAccessor(), new JdbcConnection(conn));
+         */
 
-        BufferedReader dataStr = new BufferedReader(new FileReader(sqlFile));
-        String inLine = null;
-
-        while ((inLine = dataStr.readLine()) != null) {
-            sql.append(inLine);
-        }
-
-        String[] statements = sql.toString().split(";");
-        for (String statement : statements) {
-            Statement st = _conn.createStatement();
-            st.executeUpdate(statement);
-            SqlHelper.tryToClose(st);
-        }
-
-        _fs = ChimeraFsHelper.getFileSystemProvider("test-config.xml");
+        liquibase.update("");
+        _fs = new JdbcFs(DataSources.pooledDataSource(dataSource), dbProperties.getProperty("chimera.db.dialect"));
         _rootInode = _fs.path2inode("/");
-
     }
 
     @After
@@ -51,5 +51,4 @@ public abstract class ChimeraTestCaseHelper {
         _conn.createStatement().execute("SHUTDOWN;");
         _conn.close();
     }
-
 }
--- test/org/dcache/chimera/PnfsBackwardCompatibilityTest.java
+++ test/org/dcache/chimera/PnfsBackwardCompatibilityTest.java
@@ -1,64 +1,9 @@
 package org.dcache.chimera;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
-import org.dcache.chimera.util.SqlHelper;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-
-
-public class PnfsBackwardCompatibilityTest {
-
-
-    private FileSystemProvider _fs;
-    private FsInode _rootInode;
-    private Connection _conn;
-
-    @Before
-    public void setUp() throws Exception {
-
-        Class.forName("org.hsqldb.jdbcDriver");
-
-        _conn = DriverManager.getConnection("jdbc:hsqldb:mem:chimeramem", "sa", "");
-        _conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-
-        File sqlFile = new File("sql/create-hsqldb.sql");
-        StringBuilder sql = new StringBuilder();
-
-        BufferedReader dataStr = new BufferedReader(new FileReader(sqlFile));
-        String inLine = null;
-
-        while ((inLine = dataStr.readLine()) != null) {
-            sql.append(inLine);
-        }
-
-        String[] statements = sql.toString().split(";");
-        for(String statement: statements) {
-            Statement st = _conn.createStatement();
-            st.executeUpdate(statement);
-            SqlHelper.tryToClose(st);
-        }
-
-        _fs = ChimeraFsHelper.getFileSystemProvider("test-config.xml");
-        _rootInode = _fs.path2inode("/");
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        _conn.createStatement().execute("SHUTDOWN;");
-        _conn.close();
-    }
-
+public class PnfsBackwardCompatibilityTest extends ChimeraTestCaseHelper {
 
     @Test
     public void testGetCursor() throws Exception {
