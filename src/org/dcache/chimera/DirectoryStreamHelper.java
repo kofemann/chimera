a1b4653a9ab3c4091f63b8da08230038b8042d1e
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2014-09-08 15:18:33 +0200

    chimera-core: protect list initialization from FS inconsistencies
    we observe that in some situations nlink count get out of
    sync. As this is a bug by itself, protect directory listing
    from from invalid initial capacity initialization.

--- src/org/dcache/chimera/DirectoryStreamHelper.java
+++ src/org/dcache/chimera/DirectoryStreamHelper.java
@@ -19,20 +19,34 @@ package org.dcache.chimera;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DirectoryStreamHelper {
 
+    private static final Logger _log = LoggerFactory.getLogger(DirectoryStreamHelper.class);
+
     /**
      * Convert directory stream into a {@link List}.
+     *
      * @param inode of a directory to be listed
      * @return a list of {@link HimeraDirectoryEntry}
      * @throws IOException
      */
     public static List<HimeraDirectoryEntry> listOf(FsInode inode) throws IOException, IOHimeraFsException {
 
-        List<HimeraDirectoryEntry> directoryList = new ArrayList<>(inode.statCache().getNlink());
-        try (DirectoryStreamB<HimeraDirectoryEntry> dirStream =
-                inode.newDirectoryStream()) {
+        List<HimeraDirectoryEntry> directoryList;
+
+        int estimatedListSize = inode.statCache().getNlink();
+        if (estimatedListSize < 0) {
+            _log.error("Invalid nlink count for directory {}", inode);
+            directoryList = new ArrayList<>();
+        } else {
+            directoryList = new ArrayList<>(estimatedListSize);
+        }
+
+        try (DirectoryStreamB<HimeraDirectoryEntry> dirStream
+                = inode.newDirectoryStream()) {
             for (HimeraDirectoryEntry e : dirStream) {
                 directoryList.add(e);
             }
