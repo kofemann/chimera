1085eee4a217dc9484eaebd77d228548cf5ecee4
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2020-03-18 18:54:34 +0100

    core: add DirectoryStreamHelper#streamOf
    for seamless integration with java 8 streams

--- src/org/dcache/chimera/DirectoryStreamHelper.java
+++ src/org/dcache/chimera/DirectoryStreamHelper.java
@@ -19,16 +19,19 @@ package org.dcache.chimera;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DirectoryStreamHelper {
 
     private static final Logger _log = LoggerFactory.getLogger(DirectoryStreamHelper.class);
 
     /**
-     * Convert directory stream into a {@link List}.
+     * Returns a {@link List} of {@link HimeraDirectoryEntry} in the directory {@code inode}.
      * @param inode of a directory to be listed
      * @return a list of {@link HimeraDirectoryEntry}
      * @throws IOException
@@ -44,5 +47,39 @@ public class DirectoryStreamHelper {
                 inode.newDirectoryStream()) {
             return dirStream.stream().collect(Collectors.toList());
         }
+    }
+
+    /**
+     * Returns a {@link Stream} of {@link HimeraDirectoryEntry} in the directory {@code inode}.
+     *
+     *  After this method returns, then any subsequent I/O exception that occurs while listing the directory is wrapped
+     *  in an UncheckedIOException.
+     *
+     * The returned stream keeps a db Connection. The try-with-resources construct should be used to ensure that the stream's
+     * close method is invoked after the stream operations are completed.
+     *
+     * @param inode of a directory to be listed
+     * @return a stream of {@link HimeraDirectoryEntry}
+     * @throws IOException
+     */
+    public static Stream<HimeraDirectoryEntry> streamOf(FsInode inode) throws IOException, IOHimeraFsException {
+
+        int estimatedListSize = inode.statCache().getNlink();
+        if (estimatedListSize < 0) {
+            throw new RuntimeException("Invalid nlink count for directory: " + inode);
+        }
+
+        DirectoryStreamB<HimeraDirectoryEntry> listStream = inode.newDirectoryStream();
+        return listStream.stream().onClose(uncheckedRunnable(listStream));
+    }
+
+    private static Runnable uncheckedRunnable(Closeable closeable) {
+        return () -> {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        };
     }
 }
